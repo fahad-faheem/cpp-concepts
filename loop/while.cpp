6713963c675c50e346b8e7b4bb05d48fa73381f3
Fahad Faheem <fahadfaheem3344@gmail.com>
2025-01-04 19:03:40 +0500

    while loop

---
+++ loop/while.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+// while (condition){
+// task}
+
+int main (){
+
+    int i = 0;
+
+
+    while (i < 10){
+        cout <<"The value of " << i << " is less then 10" << endl;
+        i++;
+    }
+
+    cout <<"The loop ends here because i isn't less than 10 anymore" << endl;
+
+
+
+    return 0;
+}
